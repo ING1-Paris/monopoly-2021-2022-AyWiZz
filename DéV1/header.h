12ee9f13651fdb8413a69cead5560866184c3ee1
Jojolabricot29 <95534936+Jojolabricot29@users.noreply.github.com>
2022-01-02 12:36:03 +0100

    dés

--- DéV1/header.h
+++ DéV1/header.h
@@ -2,36 +2,10 @@
 #define HEADER_H_INCLUDED
 
 
-void des(int x,int *px)
-{
 
-    int nbr = 0;
-    const int max = 6, min = 1;
-    srand(time(NULL));
-    for (int i=0; i<2; i++){
 
-        nbr = (rand() % (max - min + 1)) + min;
-        x+=nbr;
+void des(int x,int *px);
+void gotoligcol( int lig, int col );
 
-        printf("%c", 0xDA);
-        for (int j=0; j<7; j++){
-            printf("%c", 0xC4);
-        }
-        printf("%c\n", 0xBF);
-        printf("%c       %c\n", 0xB3, 0xB3);
-        printf("%c   %d   %c\n", 0xB3, nbr, 0xB3);
-        printf("%c       %c\n", 0xB3, 0xB3);
-        printf("%c", 0xC0);
-        for (int j=0; j<7; j++){
-            printf("%c", 0xC4);
-        }
-        printf("%c\n", 0xD9);
-    }
-
-    *px=x;
-
-}
-
-void des();
 
 #endif // HEADER_H_INCLUDED
