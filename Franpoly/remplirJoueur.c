c8786e27e355eb88685ae557515e19785f0d916a
GabLgv <95871677+GabLgv@users.noreply.github.com>
2022-01-09 20:42:49 +0100

    commenter ok

--- Franpoly/remplirJoueur.c
+++ Franpoly/remplirJoueur.c
@@ -3,17 +3,22 @@
 #include <windows.h>
 #define taille 20
 #include"header.h"
+///fonction permettant de remplir préalablement la structure des joueurs.
+///entrées : la structure des joueur.
+///sortie : structure des joueurs mais remplie.
 
 
 void remplir(Joueur j[])
 {
     int couleurPion = 10;
+    ///boucle permettant de remplir certains arguments de la structure
     for (int i=1; i<7; i++)
     {
         j[i].argent = 1500;
         j[i].position = 0;
         //j[i].haveToPlay = 0;
     }
+    ///boucle permettant d'assigner une couleur à chaque joueur.
     for (int i=1; i<7; i++)
     {
         j[i].couleur = couleurPion;
@@ -21,13 +26,18 @@ void remplir(Joueur j[])
     }
 
 }
+///fonction permettant le remplissage des informations par les joueurs.
+///entrées : la structure joueur.
+///sortie : le nombre de joueur.
 int remplissageJoueur(Joueur j[])
 {
     int nbJoueurs = 0;
-    int ligne = 3;
+    int ligne = 3;///variable permettant le bon placement des éléments sur la console.
     gotoligcol(2,2);
+    
     printf("Bienvenue sur le SPACE MONOPOLY !");
-
+    
+    ///boucle blindée permettant d'entrer le nombre de joueurs.
     do
     {
         gotoligcol(3,2);
@@ -42,7 +52,7 @@ int remplissageJoueur(Joueur j[])
     }
     while(nbJoueurs<1 || nbJoueurs>6);
 
-
+    ///boucle permettant d'entrer le nom des différents joueurs.
     for(int i=1; i<nbJoueurs+1; i++)
     {
         gotoligcol(ligne+i,2);
@@ -52,11 +62,15 @@ int remplissageJoueur(Joueur j[])
         gets(j[i].nom);
         ligne++;
     }
+    ///on efface la console pour pouvoir mettre le plateau.
     system("cls");
-
+    ///on retourne le nombre de joueurs
     return nbJoueurs;
 
 }
+///fonction permettant de retourner le nombre de joueur sans rappeler la fonction précedente.
+///entrées : le nombre de joueurs
+///sortie : le nombre de joueurs.
 int returnNbJ(int nbJoueurs)
 {
     int z=nbJoueurs;
