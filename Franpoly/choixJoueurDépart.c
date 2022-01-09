2c54734ead1a4ae9be786474b638c8d3d508f6a1
GabLgv <95871677+GabLgv@users.noreply.github.com>
2022-01-09 19:17:31 +0100

    commenter

--- Franpoly/choixJoueurDépart.c
+++ Franpoly/choixJoueurDépart.c
@@ -2,12 +2,17 @@
 #include <stdlib.h>
 #include <windows.h>
 #include <time.h>
+///fonction permettant de  choisir le joueur de départ
+///entrées: le nombre de joueurs
+///sortie : la selection d'un joueur qui commencera, representer par un entier.
+
+
 
 int choixDepart(nbJoueurs)
 {
     int selection = 0;
-    srand(time(NULL));
-    selection = (rand() % (nbJoueurs - 1 + 1)) + 1;
-    return selection;
+    srand(time(NULL));///on importe le temps pour avoir une génération réellement aléatoire
+    selection = (rand() % (nbJoueurs - 1 + 1)) + 1;///sélection aléatoire du joueur
+    return selection;///on renvoie le nombre
 
 }
